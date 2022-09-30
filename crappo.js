c3534e1b26e0a5878330b698ef76673969ba1aa3
MideAyo <frrezz001@gmail.com>
2022-09-30 21:02:42 +0100

    added js

--- crappo.js
+++ crappo.js
@@ -1,7 +1,9 @@
 const navBtn = document.querySelector(".btn-icon");
 const closeBtn = document.querySelector(".close-btn");
-console.log(closeBtn);
 const sideBar = document.querySelector(".side-bar");
+const singleCard = document.querySelectorAll(".single-card");
+const cryptoBtn = document.querySelectorAll(".crypto-btn");
+const cryptoIcon = document.querySelectorAll(".crypto-icon");
 
 navBtn.addEventListener("click", function () {
   ShowSideBar = sideBar.classList.contains("show-sidebar");
@@ -16,4 +18,20 @@ closeBtn.addEventListener("click", function () {
     sideBar.classList.remove("show-sidebar");
   }
   console.log(closeBtn);
+});
+
+// remove active class from each card
+function removeActive() {
+  singleCard.forEach(function (card) {
+    card.classList.remove("active");
+  });
+}
+
+// card event listener
+singleCard.forEach(function (card) {
+  card.addEventListener("click", function (e) {
+    removeActive();
+    // console.log(e.currentTarget);
+    const active = e.currentTarget.classList.add("active");
+  });
 });
